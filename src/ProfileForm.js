a74e84305f66c5717c0c9ce703cd3b611882ac4e
Michael Byon <m.byon115@gmail.com>
2018-04-20 11:32:42 +0430

    Material UI added to profile form

--- src/ProfileForm.js
+++ src/ProfileForm.js
@@ -1,81 +1,75 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {TextField} from 'material-ui';
+import Checkbox from 'material-ui/Checkbox';
+import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
+import SelectField from 'material-ui/SelectField';
 import Label from 'react-desktop/windows';
 import {Field,  reduxForm} from 'redux-form';
+import RaisedButton from 'material-ui/RaisedButton';
 import './styles/form.css';
 
+const TextArea = 
+<TextField 
+    hintText="Insert Comments"
+    multiLine={true}
+    rows={2}
+    rowsMax={5}/>
 
 let ProfileForm = props => {
     const { handleSubmit, pristine, reset, submitting } = props
     return (
       <form onSubmit={handleSubmit}>
-        <div>
+        <div className = 'formEntry'>
           <label>Age</label>
-          <div>
+          <div className = 'fieldEntry'>
             <Field
               name="age"                                
-              component="input"
+              component={TextField}
               type="text"
               placeholder="Enter Age"
             />
           </div>
         </div>
-        <div>
+        <div className = 'formEntry'>
           <label>Sex</label>
-          <div>
-            <label>
-              <Field
-                name="sex"
-                component="input"
-                type="radio"
-                value="male"
-              />{' '}
-              Male
-            </label>
-            <label>
-              <Field
-                name="sex"
-                component="input"
-                type="radio"
-                value="female"
-              />{' '}
-              Female
-            </label>
+          <div className = 'fieldEntry'>
+            <Field name="sex" component={RadioButtonGroup}>
+                <RadioButton className = 'radioButton' value="male" label="Male"/>
+                <RadioButton className = 'radioButton' value="female" label="Female"/>
+            </Field>
           </div>
         </div>
-        <div>
+        <div className = 'formEntry'>
           <label>Looking for</label>
-          <div>
-            <Field name="lookingFor" component="select">
-              <option />
+          <div className = 'fieldEntry'>
+            <Field name="lookingFor" component={SelectField}>
               <option value=""></option>
               <option value="Part-Time">Part-Time</option>
               <option value="Full-Time">Full-Time</option>
             </Field>
           </div>
         </div>
-        <div>
+        <div className = 'formEntry'>
           <label htmlFor="showAll">Show All Community Service Listings</label>
-          <div>
+          <div className = 'fieldEntry'>
             <Field
+              className = 'checkBox'
               name="showAll"
               id="showAll"
-              component="input"
+              component={Checkbox}
               type="checkbox"
             />
           </div>
         </div>
-        <div>
+        <div className = 'formEntry'>
           <label>Comments</label>
-          <div>
-            <Field name="comments" component="textarea" />
+          <div className = 'fieldEntry'>
+            <Field className = 'comments' name="comments" component={TextField} label="Notes" multiLine={true} rows={2}/>
           </div>
         </div>
         <div>
-          <button type="submit" disabled={pristine || submitting}>
-            Submit
-          </button>
+        <RaisedButton className = 'profileButton' label="Submit" primary={true} />
         </div>
       </form>
     )
