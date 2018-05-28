cd62780b6ccaa3936b81e952dd36c6a24144dd01
Michael Byon <m.byon115@gmail.com>
2018-05-28 12:57:45 +0300

    WIP Home Page

--- src/test/components/HomePage.spec.js
+++ src/test/components/HomePage.spec.js
@@ -7,20 +7,26 @@ import HomePage from '../../HomePage';
 configure({ adapter: new Adapter() });
 
 describe('Home Page with no data' ,() =>{
+    let wrapper;
+
     beforeEach(() => {
-        const props = {
+       const props = {
             points : 0,
             completedPreferences : false,
+
+            requests:{
+                requestPreferences: false,
+            }
         }
-        const wrapper = shallow(<HomePage {...props}/>);
+        wrapper = shallow(<HomePage {...props}/>);
       });
-    
+
     it('should render a div', () =>{
-        expect(wrapper.find('div')).to.exist;
+        expect(wrapper.find('div.someClass').length).to.equal(1);
     })
 
-    it('should render a pie chart of points' ,() =>{
-        expect(wrapper.find(<PieChart />).length).toBe(1);
+    it('should not render a pie chart of points' ,() =>{
+        expect(wrapper.find(<PieChart />).length).toBe(0);
     })
 
     it('should pass a prop to the pie chart on users point total', ()=>{
@@ -34,11 +40,16 @@ describe('Home Page with no data' ,() =>{
 })
 
 describe('Home Page with data' , () =>{
+    let wrapper;
 
     beforeEach(() => {
-        const props = {
+        props = {
             points : 100,
             completedPreferences : true,
+
+            requests:{
+                requestPreferences: false,
+            }
         }
         const wrapper = shallow(<HomePage {...props}/>);
       });
